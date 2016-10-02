e17e1bb44f54f67537086727892bfead3058cf03
AtecStudios <VictoryForphil@gmail.com>
2016-10-02 14:47:00 -0700

    Comments

--- server/Game.js
+++ server/Game.js
@@ -1,22 +1,25 @@
 class Game {
   constructor(host, name, password){
     this.id = Math.floor(Math.random() * 9999999) + 0  ;
-    this.host = host;
-    this.name = name;
-    this.password = password;
-    this.players = [];
-    this.currentBlackCard = {}
-    this.currentWhiteCards = [];
+    this.host = host; //Player who created it
+    this.name = name || "Open Card Game: " + id; // Game Name (Defaults to "Open Card Game: 12312" [ID of game])
+    this.password = password; // Password of game
+    this.players = []; // Players Joined
+    this.currentBlackCard = {} // Current Black card displayed
+    this.currentWhiteCards = []; // Current White cards displayed
 
-    this.players.push(host)
+    this.players.push(host) // Adds host to player list
   }
-
+  //Player Joins Rookm
   PlayerJoin(joinPacket, player){
+    //Checks is player enter correct passowrd
     if(joinPacket.password == this.password){
+      //If so add the player to the list
       this.players.push(player);
       console.log("[Game-"+this.id+"] Player Joined: " + player.name);
       console.log("[Game-"+this.id+"] Players: " + this.players.length);
     }else{
+      //Else send failed event
       console.log("[Game-"+this.id+"] Wrong Password: " + player.name);;
       player.socket.emit("join failed", {msg: "Wrong Passowrd!"});
     }
--- server/server.js
+++ server/server.js
@@ -5,26 +5,50 @@ var Game = require('./Game.js');
 var Games = [];
 var Players = [];
 
+//Sockt listens for connection event. (new client connected)
 io.on('connection', function(socket){
+  // Creates Player Class and assigned the socket to it
   var newPlayer = {
     name: "",
     socket: socket
   };
-  newPlayer.socket.on('create game', function (data) {
 
+  //Adds the player to the global player list
+  Players.push(newPlayer);
+
+  //Socket Listen for Create Game event
+  //Data should look like this:
+  /*
+    {
+      name : "Game Name",
+      password: "Game Password"
+    }
+  */
+  newPlayer.socket.on('create game', function (data) {
+    //Creates new game class and adds it to list (See Game.js)
     var newGame = new Game(newPlayer,data.name,data.password);
     Games.push(newGame);
     console.log("[Server] New Game: " + newGame.id + "-" + newGame.name);
   });
 
+  //Socket Listen for Join Game event
+  //Data should look like this:
+  /*
+    {
+      id : "Game ID",
+      password: "Game Password"
+    }
+  */
   newPlayer.socket.on('join game', function (data) {
 
+    //Gets the game via ID and joins with the player
     GetGame(data.id).PlayerJoin(data, newPlayer);
 
   });
 
 });
 
+//Gets the game from the Games List via Id
 function GetGame(id){
   var found = {};
   for (var i = 0; i < Games.length; i++) {
@@ -34,7 +58,7 @@ function GetGame(id){
   }
   return found;
 }
-
+//Starts HTTP Server on 3001
 http.listen(3001, function(){
   console.log('listening on *:3001');
 });
